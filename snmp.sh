3141f8220d691f44352b617e531a7dcf22529462
rskabc <chaer.newbie@gmail.com>
2022-07-31 17:49:46 +0700

    Update snmp.sh

--- snmp.sh
+++ snmp.sh
@@ -2,7 +2,7 @@
 apt update
 apt install -y snmp snmpd 
 mv /etc/snmp/snmpd.conf /etc/snmp/snmpd.bak
-echo "rocommunity public/nsysLocation ID, Jakarta/nsysContact Reski.abuchaer@gmail.com" > /etc/snmp/snmpd.conf
+echo "rocommunity public\nsysLocation ID, Jakarta\nsysContact Reski.abuchaer@gmail.com" > /etc/snmp/snmpd.conf
 
 /etc/init.d/snmpd restart
 snmpwalk -v 1 -c public -O e localhost
