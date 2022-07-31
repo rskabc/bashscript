8f9b6ad92d867560aaf21f773904b90e792967d2
rskabc <chaer.newbie@gmail.com>
2022-07-31 16:12:33 +0700

    Update snmp.sh

--- snmp.sh
+++ snmp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 apt update
-apt install snmp && snmpd
-mv /etc/snmp/snmpd.conf etc/snmp/snmpd.bak
+apt install snmp snmpd
+mv /etc/snmp/snmpd.conf /etc/snmp/snmpd.bak
 echo "rocommunity public" > /etc/snmp/snmpd.conf
 
 /etc/init.d/snmpd restart
