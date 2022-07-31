8c1f2b83da52c30be29b718d03e8db96a20e91e4
rskabc <chaer.newbie@gmail.com>
2022-07-31 16:07:50 +0700

    Create snmp.sh

---
+++ snmp.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt update
+apt install snmp && snmpd
+mv /etc/snmp/snmpd.conf etc/snmp/snmpd.bak
+echo "rocommunity public" > /etc/snmp/snmpd.conf
+
+/etc/init.d/snmpd restart
+snmpwalk -v 1 -c public -O e localhost
