12bebdc2a3a6254a748cf9bb05d1fca479d127b2
rskabc <chaer.newbie@gmail.com>
2022-08-03 08:58:54 +0700

    Update pathdebian.sh

--- pathdebian.sh
+++ pathdebian.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Fix PATH
-echo "PATH=$PATH:/home/pi/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/pi/.local/bin:" > |  tee -a  ~/.bashrc
+echo "PATH=$PATH:/home/pi/bin:/usr/local/sbin:/usr/sbin:/sbin:/home/pi/.local/bin:"  |  tee -a  ~/.bashrc
 
 
