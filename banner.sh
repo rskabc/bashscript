65b00cac3f102245ed6c17c13b1e4447df74dadc
rskabc <chaer.newbie@gmail.com>
2023-07-23 17:31:33 +0700

    update
    sdaa

--- banner.sh
+++ banner.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Banner /etc/ssh/banner" |  tee -a /etc/ssh/sshd_config
-wget https://raw.githubusercontent.com/rskabc/bashscript/main/banner -P /etc/ssh/
+wget https://raw.githubusercontent.com/rskabc/template/main/fornet/banner -P /etc/ssh/
 systemctl restart sshd
